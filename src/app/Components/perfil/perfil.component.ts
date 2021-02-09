acd85721cd142374ac599d2e82213b7675ecd760
Ruben <ronivenismunoz.guadalupe@alumnado.fundacionloyola.net>
2021-02-09 19:47:06 +0100

    modificaciones en el ts de crear-usuario, en el componente perfil y en estilos del navbar, 09022021 19:47

--- src/app/Components/perfil/perfil.component.ts
+++ src/app/Components/perfil/perfil.component.ts
@@ -30,7 +30,7 @@ export class PerfilComponent implements OnInit {
     this._usuariosService.getLocalidad()
     .subscribe( (localidades:any) => {
       localidades.facet_groups[1].facets.unshift({
-        name:'[Seleccione localidad]'
+        name:'Seleccione localidad'
       })
       this.localidades = localidades;
     })
@@ -38,23 +38,16 @@ export class PerfilComponent implements OnInit {
 
   formulario_modificar(){
     this.forma = this.formBuilder.group({
-      email:['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
-      pass1:['', Validators.required],
-      pass2:['', Validators.required],
-      nombre:['', [Validators.required, Validators.minLength(3)]],
-      apellido:['', [Validators.required, Validators.minLength(5)]],
-      usuario: ['', [Validators.required, Validators.minLength(5)]],
+      email:['', Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")],
+      pass1:[''],
+      pass2:[''],
+      nombre:['', Validators.minLength(3)],
+      apellido:['', Validators.minLength(5)],
+      usuario: ['', Validators.minLength(5)],
       vivienda: this.formBuilder.group({
-        direccion: ['', Validators.required],
-        ciudad: ['', Validators.required],
-        localidad: ['0', [Validators.required, Validators.min(1)]],
-        cod_postal: ['', [Validators.required, Validators.pattern("((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}")]],
+        cod_postal: ['', Validators.pattern("((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}")],
         cod_postal2: ['', Validators.pattern("((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}")]
       }),
-      direccion: ['', Validators.required],
-      ciudad: ['', Validators.required],
-      localidad: ['0', [Validators.required, Validators.min(1)]],
-      cod_postal: ['', [Validators.required, Validators.pattern("((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}")]],
       telefono: ['', Validators.pattern("[0-9]{9}")],
       tarjeta: ['', Validators.pattern(/^(?:4\d([\- ])?\d{6}\1\d{5}|(?:4\d{3}|5[1-5]\d{2}|6011)([\- ])?\d{4}\2\d{4}\2\d{4})$/)], //Visa, master y discover                                        
     },{
